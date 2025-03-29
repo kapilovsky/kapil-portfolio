f8413dd2f820c631816ae29c26093f364d236306
kapil <k4p1ll.23@gmail.com>
2025-03-29 22:41:54 +0530

    added more content in about me page

--- src/About.jsx
+++ src/About.jsx
@@ -47,22 +47,73 @@ const About = () => {
                 <hr />
                 <hr />
 
-                <div className="exp-card">
+                <div className="exp-card border-dashed border-2 p-4 my-4">
                   <div className="exp-card-header">
-                    <h2 className="redaction-35 font-bold sm:text-2xl">
+                    <h2 className="redaction-35 font-bold sm:text-2xl mt-4">
                       Full Stack Developer Intern
                     </h2>
                     <p className="redaction-35-italic sm:text-xl text-lg">
                       The Swago &bull; Sep'24 - Feb'25
                     </p>
                   </div>
-                  <div className="exp-card-content text-sm">
-                    <p>
-                      Currently working on a project that involves building a
-                      web application using React and Node.js.
+                  <div className="exp-card-content text-md">
+                    <p className="redaction-35">
+                      - Developed a Web3 dApp using React.js and Motoko,
+                      implementing Internet Identity authentication and custom
+                      token smart contracts for decentralized trading
+                      operations. <br />
+                      - Engineered full-stack features including real-time
+                      countdown systems, secure file handling for profile
+                      images, and automated token distribution mechanisms using
+                      blockchain technology. <br />- Built and integrated smart
+                      contracts for secure token transfers and trading
+                      operations, implementing proper authorization checks and
+                      maintaining data integrity across the DFINITY's Internet
+                      Computer blockchain
+                    </p>
+                    <p className="redaction-35 mt-4">
+                      Tech Stack: React.js, Tailwind CSS, Motoko, DFINITY's
+                      Internet Computer
                     </p>
                   </div>
                 </div>
+
+                <div className="exp-card border-dashed border-2 p-4 mb-4">
+                  <div className="exp-card-header">
+                    <h2 className="redaction-35 font-bold sm:text-2xl mt-4">
+                      Web Development Intern
+                    </h2>
+                    <p className="redaction-35-italic sm:text-xl text-lg">
+                      Design Innovation Centre (DIC) Lab &bull; Jun'24 – Jul'24
+                    </p>
+                  </div>
+                  <div className="exp-card-content text-md">
+                    <p className="redaction-35">
+                      - Developed a responsive website for TechViz, a tool
+                      enhancing spatial awareness for the visually impaired.{" "}
+                      <br />- Customized Google Forms which typically doesn't
+                      allow CSS customization when embedded. This hack enabled
+                      me to create a tailored form while retaining all the
+                      powerful features of Google Forms, including:
+                      <br />
+                      &bull; Email notifications <br /> &bull; Data storage in
+                      Google Sheets
+                      <br /> &bull; Scalability <br /> &bull; Unlimited
+                      responses <br />
+                    </p>
+                    <p className="redaction-35 mt-4">
+                      Tech Stack: Next.js + Framer
+                    </p>
+                  </div>
+                </div>
+
+                <hr />
+                <hr />
+                <h4 className="redaction-35 text-center sm:text-2xl text-lg">
+                  Volunteering
+                </h4>
+                <hr />
+                <hr />
 
                 <p className="text-sm">
                   Hello! I'm a developer passionate about creating unique
